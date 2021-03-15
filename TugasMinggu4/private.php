c0cd74f5eedc6ca6a3726623d21eaba9dd8b4e9c
AnaNur1009 <ananurrohma09@gmail.com>
2021-03-15 09:47:01 +0700

    Create private.php

---
+++ TugasMinggu4/private.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Car{
+	private $model;
+
+	public function setModel($model){
+		$this->model = $model;
+	}
+	
+}
+
+class sportsCar extends Car{
+	public function hello()
+	{
+		return "Beep I am a <i>". $this->model."</i><br/>";
+	}
+
+}
+
+$sportsCar1 = new sportsCar();
+
+$sportsCar1->setModel('Mercedes Benz');
+
+echo $sportsCar1->hello();
+?>
